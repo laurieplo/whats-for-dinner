6826368a29b8669e68975d3e5771f11bc596a685
laurieplo <laurie.plociennik@gmail.com>
2021-10-26 20:58:30 +0200

    Use className instead of css (#19)

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,5 +1,3 @@
-/** @jsxImportSource @emotion/react */
-
 import logo from '../../logo.svg';
 import * as styles from './Dashboard.styles';
 import { useMenuReferential } from './hooks/useMenuReferential';
@@ -9,20 +7,20 @@ export function Dashboard() {
 
     return (
         <div>
-            <header css={styles.header}>
-                <div css={styles.logo}>
-                    <img src={logo} css={styles.logoImg} alt="logo" />
-                    <p css={styles.title}>A taaaaable !</p>
+            <header className={styles.header}>
+                <div className={styles.logo}>
+                    <img src={logo} className={styles.logoImg} alt="logo" />
+                    <p className={styles.title}>A taaaaable !</p>
                 </div>
                 <button
-                    css={styles.button}
+                    className={styles.button}
                     onClick={generateWeekMenu}
                     data-cy='action-generate-menu'
                 >
                     Générer le menu de la semaine
                 </button>
             </header>
-            <div css={styles.content}>
+            <div className={styles.content}>
                 { weekMenu.map(meal => <div data-cy='menu-item'>{ meal }</div>)}
             </div>
         </div>
